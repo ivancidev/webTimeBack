fb3f1a9c2ee0d97d05c2176d4acb798a7fda91e2
Bruno Salcedo <bruno.salcedo.ca@gmail.com>
2024-10-01 18:52:25 -0400

    feat(books): Implement POST route to add books (DB logic not implement yet)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ const app = express();
 const port = process.env.PORT ;
 const apiKey = process.env.API_KEY;
 
+
+//Para realizar la petición post
+app.use(express.json());
+
+app.post('/books', (req, res) => {
+    const { titulo, autor, categoria, idioma, sinopsis } = req.body;
+    console.log('Datos del libro recibidos:', { titulo, autor, categoria, idioma, sinopsis });
+  
+    //Aqui pones el codigo para registrarlo en la base de datos
+  
+    res.status(201).send('Libro agregado correctamente');
+  });
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello Nodemon!');
 })
