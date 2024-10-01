c051f73b9654f89533cc19219ede2714ea31f5de
Bruno Salcedo <bruno.salcedo.ca@gmail.com>
2024-09-30 22:45:42 -0400

    feat: instalar dotenv y nodemon en el proyecto

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT ;
+const apiKey = process.env.API_KEY;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello Nodemon!');
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
+
+console.log(`La clave API es: ${apiKey}`);
