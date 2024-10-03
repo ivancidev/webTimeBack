217a26650660e89c303fdde86e322cdf01692849
KraAlv <santito.sfg@gmail.com>
2024-10-02 21:04:44 -0400

    feat: despliegue Nodejs Render

--- src/database/connectionPostgreSQL.js
+++ src/database/connectionPostgreSQL.js
@@ -1,8 +1,9 @@
 import pg from "pg"
+
 export const pool = new pg.Pool({
     host:"localhost",
-    port:"5432",
-    database:"webTime_pg",
+    port:5432,
+    database:"webtime",
     user:"postgres",
-    password:"qwerasdf",
+    password:"delunoalocho",
 })
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-//const express = require('express');
-//const app = express();
-//const port = 3000;
+// const express = require('express');
+import express from "express"
+const app = express();
+const port = 3000;
 import { pool } from "./database/connectionPostgreSQL.js"
 
 const getLibro= async ()=>{
     try{
-        const result = await pool.query(`SELECT * FROM "LIBRO"`)
+        const result = await pool.query(`SELECT * FROM libro`)
         console.log(result)
     }catch(error){
         console.error(error)
     }
 }
+app.get('/',(req, res)=>{
+    res.send("Hola Mundo")
+})
 getLibro();
+app.listen(3000)
+
