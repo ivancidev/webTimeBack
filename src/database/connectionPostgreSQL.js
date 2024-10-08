f3b5399434436c95be519ccd07ad4e7a10d34ca0
ivancidev <herlanherbas@gmail.com>
2024-10-08 07:52:22 -0400

    Refactor database connection configuration for PostgreSQL

--- src/database/connectionPostgreSQL.js
+++ src/database/connectionPostgreSQL.js
@@ -1,8 +1,14 @@
-import pg from "pg"
-export const pool = new pg.Pool({
-    host:"localhost",
-    port:"5432",
-    database:"webTime_pg",
-    user:"postgres",
-    password:"qwerasdf",
-})
+const { Pool } = require("pg");
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+pool.on("error", (err) => {
+  console.error("Error en la conexión a la base de datos:", err);
+});
+
+module.exports = { pool };
