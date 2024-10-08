1bb459987b0729286474e28e32e3983a98bb63e8
ivancidev <herlanherbas@gmail.com>
2024-10-08 07:52:54 -0400

    Refactor database connection configuration for PostgreSQL and update book-routes.js

--- src/routes/book-routes.js
+++ src/routes/book-routes.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import { getLanguages, getAuthors, getCategories } from '../controllers/book-controller.js'
+const express = require('express');
+const { getAuthors, getCategories, getLanguages, addBook } = require('../controllers/book-controller');
 
 const router = express.Router();
 
+router.post("/books", addBook);
 router.get('/languages', getLanguages);
 router.get('/authors', getAuthors);
 router.get('/categories', getCategories);
-export default router;
+
+module.exports = router;
